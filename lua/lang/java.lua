43b7c9630f8720f0c4d249b7f54798ac2b71178f
Gakawarstone <pinnekerv@gmail.com>
2025-04-06 18:58:50 +0200

    fix: java

--- lua/lang/java.lua
+++ lua/lang/java.lua
@@ -6,4 +6,15 @@ lsp.jdtls.setup({
 	on_attach = On_attach,
 })
 
-fmt.formatters_by_ft.java = { "google-java-format" }
+-- fmt.formatters_by_ft.java = { "google-java-format" }
+fmt.formatters_by_ft.java = { "clang-format" }
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		-- Example: Disable for TypeScript/JavaScript (tsserver)
+		if client.name == "jdtls" then
+			client.server_capabilities.semanticTokensProvider = nil
+		end
+	end,
+})
