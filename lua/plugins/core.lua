fb5818ff1834b05c65d1f3824928b319aaa71f18
Gakawarstone <pinnekerv@gmail.com>
2026-03-29 14:14:31 +0200

    fix: telescope search with hidden files

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -17,6 +17,26 @@ return {
 		"nvim-telescope/telescope.nvim",
 		branch = "0.1.x",
 		dependencies = { { "nvim-lua/plenary.nvim" } },
+		opts = {
+			pickers = {
+				find_files = {
+					hidden = true,
+				},
+			},
+			defaults = {
+				vimgrep_arguments = {
+					"rg",
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case",
+					"--hidden",
+					"--glob=!.git/*",
+				},
+			},
+		},
 	},
 
 	"stevearc/conform.nvim",
