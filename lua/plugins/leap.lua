ae8ee55681b92b1b6576126a4819f363e8da9c7f
Gakawarstone <pinnekerv@gmail.com>
2026-03-31 14:19:27 +0200

    fix: leap.nvim outdated config

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,6 +1,9 @@
 return {
-	"ggandor/leap.nvim",
+	"andyg/leap.nvim",
+	url = "https://codeberg.org/andyg/leap.nvim",
 	config = function()
-		require("leap").add_default_mappings()
+		vim.keymap.set({'n', 'x', 'o'}, 's',  '<Plug>(leap-forward)')
+		vim.keymap.set({'n', 'x', 'o'}, 'S',  '<Plug>(leap-backward)')
+		vim.keymap.set({'n', 'x', 'o'}, 'gs', '<Plug>(leap-from-window)')
 	end,
 }
