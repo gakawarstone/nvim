314fb7cf2fdf6c3053096198a01a0252ce2336db
Gakawarstone <pinnekerv@gmail.com>
2026-04-03 13:13:07 +0200

    chore: refactor treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,30 +1,29 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		branch = "master",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = {
-					"lua",
-					"python",
-					"kotlin",
-					"v",
-					"solidity",
-					"markdown",
-					"typescript",
-					"scss",
-					"html",
-					"javascript",
-					"css",
-					"go",
-					"rust",
-					"angular",
-				},
-				auto_install = true,
-				highlight = {
-					enable = true,
-				},
-			})
-		end,
+		build = ":TSUpdate",
+		main = "nvim-treesitter.configs",
+		opts = {
+			ensure_installed = {
+				"lua",
+				"python",
+				"kotlin",
+				"v",
+				"solidity",
+				"markdown",
+				"typescript",
+				"scss",
+				"html",
+				"javascript",
+				"css",
+				"go",
+				"rust",
+				"angular",
+			},
+			auto_install = true,
+			highlight = {
+				enable = true,
+			},
+		},
 	},
 }
