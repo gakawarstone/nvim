d5983a7dd4ee681a8c886613748619869243ca5b
Gakawarstone <pinnekerv@gmail.com>
2026-04-03 11:47:16 +0200

    feat: use snacks input

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -13,6 +13,17 @@ return {
 			enabled = true,
 			layout = "telescope",
 		},
+		input = {
+			enabled = true,
+			win = {
+				relative = "cursor",
+				width = function(win)
+					return math.max(40, #(win.opts.prompt or "") + 20)
+				end,
+				row = 1,
+				col = 0,
+			},
+		},
 		matcher = {
 			frecency = true, -- frecency bonus
 		},
