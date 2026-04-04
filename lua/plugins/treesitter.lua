8c1c6025fba4a299163c00f819fa218afc8b164f
Gakawarstone <pinnekerv@gmail.com>
2026-04-04 11:33:54 +0200

    fix: remove ensure installed from tressitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,22 +4,6 @@ return {
 		build = ":TSUpdate",
 		main = "nvim-treesitter.configs",
 		opts = {
-			ensure_installed = {
-				"lua",
-				"python",
-				"kotlin",
-				"v",
-				"solidity",
-				"markdown",
-				"typescript",
-				"scss",
-				"html",
-				"javascript",
-				"css",
-				"go",
-				"rust",
-				"angular",
-			},
 			auto_install = true,
 			highlight = {
 				enable = true,
