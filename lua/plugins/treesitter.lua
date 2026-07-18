a4275b26d71ab97341e2b43beaa8aaf26c8aa9a8
Gakawarstone <pinnekerv@gmail.com>
2026-07-18 14:16:11 +0200

    fix: support treesitter on neovim 0.12

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,29 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		branch = "master",
-		main = "nvim-treesitter.configs",
+		branch = "main",
 		lazy = false,
 		build = ":TSUpdate",
-		opts = {
-			auto_install = true,
-			highlight = {
-				enable = true,
-			},
-		},
+		config = function()
+			local treesitter = require("nvim-treesitter")
+			treesitter.setup()
+
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function(args)
+					local lang = vim.treesitter.language.get_lang(vim.bo[args.buf].filetype)
+					if not lang then
+						return
+					end
+
+					if not vim.treesitter.language.add(lang) and vim.tbl_contains(treesitter.get_available(), lang) then
+						treesitter.install(lang):await(300000)
+					end
+
+					if vim.treesitter.language.add(lang) then
+						vim.treesitter.start(args.buf, lang)
+					end
+				end,
+			})
+		end,
 	},
 }
